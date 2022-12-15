902dabb04e463ee8d20e2a98a83dbdaee5a40cdd
yianzhou <2873642764@qq.com>
2022-12-15 11:27:17 +0800

    测试

--- src/test/java/com/yian/test/TestDome.java
+++ src/test/java/com/yian/test/TestDome.java
@@ -1,8 +1,15 @@
 package com.yian.test;
 
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Test;
+import com.atyian.pet.admin.controller.request.CommonPageRequest;
+import com.atyian.pet.admin.mapper.UserMapper;
+import com.atyian.pet.admin.pojo.User;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.util.List;
 
 
 /**
@@ -11,9 +18,17 @@ import org.junit.jupiter.api.Test;
  * @desc:
  * @datetime: 2022-12-13-17:23
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:applicationContext.xml")
 public class TestDome {
+    @Autowired
+    private UserMapper userService;
     @Test
     public void test(){
-
+        CommonPageRequest commonPageRequest = new CommonPageRequest();
+        commonPageRequest.setPageNum(1);
+        commonPageRequest.setPageSize(5);
+        List<User> users = userService.selectAllUserByCondition(commonPageRequest);
+        System.out.println(users);
     }
 }
